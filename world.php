910194d89b733de6713f045555dffd9c1a35ce6c
Pascale Gordon <pascale_gordon@hotmail.com>
2020-11-20 01:49:53 -0500

    exercise 4

--- world.php
+++ world.php
@@ -15,8 +15,46 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<?php>
+
+$stmt = $conn->query("SELECT name, continent, independence, head_of_state FROM countries");
+
+echo "<table border='1'>
+
+<tr>
+
+<th>Name</th>
+
+<th>Continent</th>
+
+<th>Independence</th>
+
+<th>Head of state </th>
+
+</tr>";
+
+while($row = mysql_fetch_array($result))
+
+  {
+
+   "<tr>";
+
+   "<td>" . $row['name'] . "</td>";
+
+   "<td>" . $row['continent'] . "</td>";
+
+   "<td>" . $row['independence'] . "</td>";
+
+   "<td>" . $row['head of state'] . "</td>";
+
+   "</tr>";
+
+  }
+
+echo "</table>";
+
+ 
+
+
+
+?>
